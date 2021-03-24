800f1b63f52670eb746d14333ad5b02597ad6473
BPluijm <75165735+BPluijm@users.noreply.github.com>
2021-03-24 20:09:18 +0100

    feature/uitwerkingen

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import PageHeader from '../components/PageHeader';
 import logo from '../assets/toothbrushes.svg';
 
 function HomePage() {
+    const history = useHistory();
+
+    function handleClick() {
+        history.push('/afspraak-maken');
+    }
+
   return (
-    <div className="page-container">
+     <div className="page-container">
       <PageHeader icon={logo} title="Tandartspraktijk de Tandenborstel" />
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut debitis doloribus
@@ -22,6 +28,9 @@ function HomePage() {
         similique sint vero? Accusamus aliquam aliquid blanditiis consequatur est et minima mollitia neque non, odit perspiciatis placeat
         provident quos, similique sit totam vero. Beatae consequatur cupiditate rerum?
       </p>
+         <button type="button" onClick={handleClick}>
+             Maak gelijk een afspraak!
+         </button>
       <p>Leer <Link to="/tanden-bleken">hier</Link> meer over het bleken van tanden</p>
     </div>
   );
